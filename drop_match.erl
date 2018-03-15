74ecaaa70fb320a50d3ef3400ea4ab77d40740d1
Pierre Fenoll <pierrefenoll@gmail.com>
2018-03-15 17:55:09 +0100

    drop_match: interesting, apply and match are ordered at the assembly level

--- drop_match.erl
+++ drop_match.erl
@@ -1,5 +1,6 @@
 -module(drop_match).
 -export([a/0,b/0]).
+-export([aa/0,bb/0]).
 
 a() ->
     _ = {erlang:erase(a), erlang:erase(b)},
@@ -8,4 +9,13 @@ a() ->
 b() ->
     _ = erlang:erase(a),
     _ = erlang:erase(b),
+    ok.
+
+aa() ->
+    {1,2} = {erlang:get(a), erlang:get(b)},
+    ok.
+
+bb() ->
+    1 = erlang:get(a),
+    2 = erlang:get(b),
     ok.
