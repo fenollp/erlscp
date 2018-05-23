c5b869efc0bdc8cdbb2944da76058618814e130d
Pierre Fenoll <pierrefenoll@gmail.com>
2018-05-23 16:08:43 +0200

    concat: chaining to/from_atom works though!

--- concat.erl
+++ concat.erl
@@ -1,12 +1,16 @@
 -module(concat).
--export([a0/0, a1/0, b0/0, b1/0]).
+-export([a0/0,a1/0,a2/0, b0/0,b1/0,b2/0]).
 
 a0() ->
     atom_to_list(tom) ++ "tom".
 a1() ->
     lists:concat([tom,"tom"]).
+a2() ->
+    list_to_atom(atom_to_list(tom)++"tom").
 
 b0() ->
     "tomtom".
 b1() ->
     "tomtom".
+b2() ->
+    tomtom.
